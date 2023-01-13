67163916ed768de1b5ed444de6aecd3342d3ded4
Rod <you@gmail.com>
2023-01-13 04:32:29 -0300

    I deployed a bank landing page

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import NavBar from "./NavBar";
+import Hero from "./Hero";
+import Billing from './Billing'
+import Bussines from './Bussines'
+import CTA from './CTA'
+import Client from './Client'
+import CardDeal from './CardDeal'
+import Footer from './Footer'
+import Stats from './Stats'
+import Testimonials from './Testimonials'
+
+export {
+    NavBar,
+    Hero,
+    Billing,
+    Bussines,
+    CTA,
+    Client,
+    CardDeal,
+    Footer,
+    Stats,
+    Testimonials
+}
